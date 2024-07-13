a4d6dc5f2ca1f4d8fff03172971cb5458528a39d
Idea Phakpaphon <ideapakpaphon@gmail.com>
2024-07-13 18:27:10 +0700

    Shikanokano

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,41 @@
 
 
 
+<style>
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 25%;
+    }
+
+    .container2 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 30%;
+    }
+  
+    .container img {
+      max-width: 100%;
+      height: auto;
+      object-fit: contain;
+    }
+  </style>
 
 <div class="mt-5 flex flex-col space-y-5 items-center justify-center sm:1/2">
 
 
-        <h1 class="text-5xl font-bold">Nokotan Escape</h1>
-
+        <h1 class="text-5xl font-bold">Nokotan Escape 🗣️🚨🦌🦌</h1>
+        <p>Shikanoko Nokonoko Koshitantan Shikanoko Nokonoko Koshitantan</p>
         <a href="/nokotangaming">
             <button class="btn">Shikanokanos</button>
         </a>
+
+        <div class="w-full items-center container">
+            <img src="/images/01.png" alt="Logo" />
+            <img src="/images/02.png" alt="Logo" />
+        </div>
         
    
 </div>
--- src/routes/nokotangaming/+page.svelte
+++ src/routes/nokotangaming/+page.svelte
@@ -1,20 +1,34 @@
 <script>
 
-
-
   export let data;
+
+  async function FollowNokotan () {
+
+  }
+
+  async function FollowKaewkap () {
+
+  }
+
+  async function SpeedUp () {
+
+  }
+
+  async function SpawnDeer () {
+
+  }
 </script>
 
 
 
 <div class="mt-5 flex flex-col space-y-5 items-center justify-center sm:1/2">
 
-    <button class="btn">Toggle to follow nokotan</button>
-    <button class="btn">Toggle to follow kaewkap</button>
-    <button class="btn">Move nokotan</button>
-    <button class="btn">Move nokotan</button>
-    <button class="btn">Move nokotan</button>
+    <button class="btn" on:click={FollowNokotan}>Toggle to follow nokotan</button>
+    <button class="btn" on:click={FollowKaewkap}>Toggle to follow kaewkap</button>
+    <button class="btn" on:click={SpeedUp}>SpeedUp nokotan</button>
+    <button class="btn" on:click={SpawnDeer}>Spawn Deer</button>
+    <button class="btn">Todo</button>
 
-    <p class="mt-4">Message: {data.message}</p>
-    <p>Timestamp: {data.timestamp}</p>
+    <!-- <p class="mt-4">Message: {data.message}</p>
+    <p>Timestamp: {data.timestamp}</p> -->
 </div>
