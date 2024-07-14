4077ce015d4d298100df906f58c9327632976c51
pontakornth <pontakorn61@gmail.com>
2024-07-14 14:23:16 +0700

    Push

--- src/routes/api/move/+server.ts
+++ src/routes/api/move/+server.ts
@@ -1,12 +1,23 @@
 // src/routes/api/data/+server.js
-export async function GET() {
-    const data = {
-      message: 'Hello, SvelteKit API!',
-      timestamp: new Date().toISOString(),
-    };
-  
-    return new Response(JSON.stringify(data), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  
+
+import { json } from "@sveltejs/kit"
+import type { RequestHandler } from "./$types"
+
+// Please don't do this in the actual SvelteKit app.
+let data = {
+  ai: 1,
+  skill: 0,
+  timestamp: 0
+}
+export const GET: RequestHandler = async ({ url }) => {
+  return json(data)
+}
+
+export const POST: RequestHandler = async ({ request }) => {
+  const body =await request.json()
+
+  data.ai = body["ai"]
+  data.skill = body["skill"]
+  data.timestamp = Date.now()
+  return json(data)
+}
--- src/routes/nokotangaming/+page.svelte
+++ src/routes/nokotangaming/+page.svelte
@@ -1,74 +1,104 @@
 <script>
+	let data = {
+		ai: 1,
+		skill: 0
+	};
+	function onChangeAi(e) {
+		data.ai = parseInt(e.currentTarget.value);
+	}
 
-  export let data;
-  let selectedOption = 'AI Control';
+	function onChangeSkill(e) {
+		data.skill = parseInt(e.currentTarget.value);
+	}
 
-  async function FollowNokotan () {
-
-  }
-
-  async function FollowKaewkap () {
-
-  }
-
-  async function SpeedUp () {
-
-  }
-
-  async function SpawnDeer () {
-
-  }
+	async function submitRequest() {
+		const response = await fetch('/api/move', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		});
+		const result = await response.json();
+		console.log(result);
+	}
 </script>
 
-
-
 <div class="mt-5 flex flex-col space-y-5 items-center justify-center sm:1/2">
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">Follow Koshitan</span>
+			<input
+				type="radio"
+				value="1"
+				name="radio-10"
+				on:change={onChangeAi}
+				class="radio checked:bg-red-500"
+				checked={data.ai == 1}
+			/>
+		</label>
+	</div>
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">Follow Crisp</span>
+			<input
+				type="radio"
+				value="2"
+				name="radio-10"
+				on:change={onChangeAi}
+				class="radio checked:bg-blue-500"
+				checked={data.ai == 2}
+			/>
+		</label>
+	</div>
 
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">No skill</span>
+			<input
+				type="radio"
+				value="0"
+				name="radio-1"
+				on:change={onChangeSkill}
+				class="radio checked:bg-red-500"
+				checked={data.skill == 0}
+			/>
+		</label>
 
-  <div class="form-control">
-    <label class="label cursor-pointer">
-      <span class="label-text">Follow Koshitan</span>
-      <input type="radio" name="radio-10" class="radio checked:bg-red-500" checked="checked" />
-    </label>
-  </div>
-  <div class="form-control">
-    <label class="label cursor-pointer">
-      <span class="label-text">Follow Crisp</span>
-      <input type="radio" name="radio-10" class="radio checked:bg-blue-500" checked="checked" />
-    </label>
-  </div>
+		<label class="label cursor-pointer">
+			<span class="label-text">Skill 1</span>
+			<input
+				type="radio"
+				value="1"
+				name="radio-1"
+				on:change={onChangeSkill}
+				class="radio checked:bg-blue-500"
+				checked={data.skill == 1}
+			/>
+		</label>
+		<label class="label cursor-pointer">
+			<span class="label-text">Skill 2</span>
+			<input
+				type="radio"
+				value="2"
+				name="radio-1"
+				on:change={onChangeSkill}
+				class="radio checked:bg-blue-500"
+				checked={data.skill == 2}
+			/>
+		</label>
+		<label class="label cursor-pointer">
+			<span class="label-text">Skill 3</span>
+			<input
+				type="radio"
+				value="3"
+				name="radio-1"
+				on:change={onChangeSkill}
+				class="radio checked:bg-blue-500"
+				checked={data.skill == 3}
+			/>
+		</label>
+	</div>
 
-  
-  <div class="form-control">
-    <label class="label cursor-pointer">
-      <span class="label-text">No skill</span>
-      <input type="radio" name="radio-1" value="NoSkill" bind:group={selectedOption} class="radio checked:bg-red-500" checked="checked" />
-    </label>
- 
-    <label class="label cursor-pointer">
-      <span class="label-text">Skill 1</span>
-      <input type="radio" name="radio-1" value="Skill01" bind:group={selectedOption} class="radio checked:bg-blue-500" checked="checked" />
-    </label>
-    <label class="label cursor-pointer">
-      <span class="label-text">Skill 2</span>
-      <input type="radio" name="radio-1" value="Skill02" bind:group={selectedOption} class="radio checked:bg-blue-500" checked="checked" />
-    </label>
-    <label class="label cursor-pointer">
-      <span class="label-text">Skill 3</span>
-      <input type="radio" name="radio-1" value="Skill03" bind:group={selectedOption} class="radio checked:bg-blue-500" checked="checked" />
-    </label>
-  </div>
-
-    <button class="btn" on:click={FollowNokotan}>Submit</button>
-    <!-- <button class="btn" on:click={FollowKaewkap}>Toggle to follow kaewkap</button>
-
-    <button class="btn" on:click={SpeedUp}>SpeedUp nokotan</button>
-    <button class="btn" on:click={SpawnDeer}>Spawn Deer</button>
-    <button class="btn">Todo</button> -->
-
-
-    
-
-    <!-- <p class="mt-4">Message: {data.message}</p>
-    <p>Timestamp: {data.timestamp}</p> -->
+	<button class="btn" on:click={submitRequest}>Submit</button>
 </div>
