09fcc83446955cff7d28e213748a3719ac8c6236
Idea Phakpaphon <ideapakpaphon@gmail.com>
2024-07-14 13:23:02 +0700

    Push

--- src/routes/nokotangaming/+page.svelte
+++ src/routes/nokotangaming/+page.svelte
@@ -27,31 +27,46 @@
 
   <div class="form-control">
     <label class="label cursor-pointer">
-      <span class="label-text">AI control</span>
-      <input type="radio" name="radio-10" value="AI Control" bind:group={selectedOption} class="radio checked:bg-red-500" checked="checked" />
+      <span class="label-text">Follow Koshitan</span>
+      <input type="radio" name="radio-10" class="radio checked:bg-red-500" checked="checked" />
     </label>
   </div>
   <div class="form-control">
     <label class="label cursor-pointer">
-      <span class="label-text">skills</span>
-      <input type="radio" name="radio-10" value="Skills" bind:group={selectedOption} class="radio checked:bg-blue-500" checked="checked" />
+      <span class="label-text">Follow Crisp</span>
+      <input type="radio" name="radio-10" class="radio checked:bg-blue-500" checked="checked" />
     </label>
   </div>
 
-  {#if selectedOption == 'AI Control'}
-  <button class="btn" on:click={FollowNokotan}>Toggle to follow nokotan</button>
-  <button class="btn" on:click={FollowKaewkap}>Toggle to follow kaewkap</button>
+  
+  <div class="form-control">
+    <label class="label cursor-pointer">
+      <span class="label-text">No skill</span>
+      <input type="radio" name="radio-1" value="NoSkill" bind:group={selectedOption} class="radio checked:bg-red-500" checked="checked" />
+    </label>
+ 
+    <label class="label cursor-pointer">
+      <span class="label-text">Skill 1</span>
+      <input type="radio" name="radio-1" value="Skill01" bind:group={selectedOption} class="radio checked:bg-blue-500" checked="checked" />
+    </label>
+    <label class="label cursor-pointer">
+      <span class="label-text">Skill 2</span>
+      <input type="radio" name="radio-1" value="Skill02" bind:group={selectedOption} class="radio checked:bg-blue-500" checked="checked" />
+    </label>
+    <label class="label cursor-pointer">
+      <span class="label-text">Skill 3</span>
+      <input type="radio" name="radio-1" value="Skill03" bind:group={selectedOption} class="radio checked:bg-blue-500" checked="checked" />
+    </label>
+  </div>
 
-
-  {:else}
+    <button class="btn" on:click={FollowNokotan}>Submit</button>
+    <!-- <button class="btn" on:click={FollowKaewkap}>Toggle to follow kaewkap</button>
 
     <button class="btn" on:click={SpeedUp}>SpeedUp nokotan</button>
     <button class="btn" on:click={SpawnDeer}>Spawn Deer</button>
-    <button class="btn">Todo</button>
+    <button class="btn">Todo</button> -->
 
 
-  {/if}
-   
     
 
     <!-- <p class="mt-4">Message: {data.message}</p>
